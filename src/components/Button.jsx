d1cbcbf135dd2cedd1aa7fb80d76e52626e53cde
Pat <sipapat.prasopsap@gmail.com>
2020-11-04 16:02:19 +0100

    Added customerList page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const StyledButton = styled.button`
     background-color: #4CAF50; 
@@ -12,10 +12,8 @@ const StyledButton = styled.button`
     font-size: 16px;
     border-radius: 5px;
     margin-top 25px;
-`
+`;
 
 export default function Button() {
-    return (
-        <StyledButton>Login</StyledButton>
-    )
+  return <StyledButton>Login</StyledButton>;
 }
